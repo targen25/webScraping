0d7fcd38604b1b43de72bf350e0cada757242e76
andres <ogti_temp59@produce.gob.pe>
2020-01-24 10:02:57 -0500

    version 1

---
+++ webScraping/webScraping/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace webScraping
+{
+    partial class fmWebScraping
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvResult = new System.Windows.Forms.DataGridView();
+            this.buSearch = new System.Windows.Forms.Button();
+            this.txSearch = new System.Windows.Forms.TextBox();
+            this.laSearch = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvResult
+            // 
+            this.dgvResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResult.Location = new System.Drawing.Point(127, 97);
+            this.dgvResult.Name = "dgvResult";
+            this.dgvResult.RowTemplate.Height = 24;
+            this.dgvResult.Size = new System.Drawing.Size(553, 154);
+            this.dgvResult.TabIndex = 0;
+            // 
+            // buSearch
+            // 
+            this.buSearch.Location = new System.Drawing.Point(127, 296);
+            this.buSearch.Name = "buSearch";
+            this.buSearch.Size = new System.Drawing.Size(75, 23);
+            this.buSearch.TabIndex = 1;
+            this.buSearch.Text = "Buscar";
+            this.buSearch.UseVisualStyleBackColor = true;
+            this.buSearch.Click += new System.EventHandler(this.buSearch_Click);
+            // 
+            // txSearch
+            // 
+            this.txSearch.Location = new System.Drawing.Point(299, 50);
+            this.txSearch.Name = "txSearch";
+            this.txSearch.Size = new System.Drawing.Size(268, 22);
+            this.txSearch.TabIndex = 2;
+            // 
+            // laSearch
+            // 
+            this.laSearch.AutoSize = true;
+            this.laSearch.Location = new System.Drawing.Point(124, 53);
+            this.laSearch.Name = "laSearch";
+            this.laSearch.Size = new System.Drawing.Size(159, 17);
+            this.laSearch.TabIndex = 3;
+            this.laSearch.Text = "Enter the text to search:";
+            // 
+            // fmWebScraping
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.laSearch);
+            this.Controls.Add(this.txSearch);
+            this.Controls.Add(this.buSearch);
+            this.Controls.Add(this.dgvResult);
+            this.Name = "fmWebScraping";
+            this.Text = "Web Scraping";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvResult;
+        private System.Windows.Forms.Button buSearch;
+        private System.Windows.Forms.TextBox txSearch;
+        private System.Windows.Forms.Label laSearch;
+    }
+}
+
---
+++ webScraping/webScraping/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace webScraping
+{
+    public partial class fmWebScraping : Form
+    {
+        static DataTable table;
+        static int inputYahoo = 0;
+        static int inputBind = 0;
+
+        public fmWebScraping()
+        {
+            InitializeComponent();
+            IniTable();
+        }
+
+        private void IniTable()
+        {
+            table = new DataTable();
+            table.Columns.Add("Search Engines Yahoo", typeof(string));
+            table.Columns.Add("Search Engines Bind", typeof(string));
+            table.Columns.Add("Winner", typeof(string));
+            dgvResult.DataSource = table;
+        }
+        private void buSearch_Click(object sender, EventArgs e)
+        {
+            var input = "\""+ txSearch.Text+"\"";
+            //GetHtmlYahoo(input);
+            //GetHtmlBing(input);
+            GetHtml(input);
+            table.Rows.Add(inputYahoo, inputBind, "");
+        }
+        static async void GetHtmlYahoo(string input)
+        {
+
+            var url = "https://search.yahoo.com/search?p=" + input;
+
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+            var htmlDocument = new HtmlAgilityPack.HtmlDocument();
+
+            
+            string string1 = html;
+            string toFind1 = "Next</a><span>";
+            int start = string1.IndexOf(toFind1) + toFind1.Length;
+            int end = start + 40;
+            string string2 = string1.Substring(start, end - start);
+
+            string onlyNumber = Regex.Replace(string2, "[a-zA-Z</>,\n. ]", "");
+            inputYahoo = Convert.ToInt32(onlyNumber);
+
+            table.Rows[0][0] = inputYahoo;
+            
+        }
+        static async void GetHtmlBing(string input)
+        {
+
+            var url = "https://www.bing.com/search?q="+ input ;
+
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+            
+            string string1 = html;
+            string toFind1 = "class=\"sb_count\"";
+            int start = string1.IndexOf(toFind1) + toFind1.Length;
+            int end = start + 40;
+            string string2 = string1.Substring(start, end - start);
+
+            string onlyNumber = Regex.Replace(string2, "[a-zA-Z</>,\n. ]", "");
+            inputBind = Convert.ToInt32(onlyNumber);
+
+            table.Rows[0][1] = inputBind;
+
+
+        }
+
+        static async void GetHtml(string input)
+        {
+
+            //----Search on Yahoo
+            string urlYahoo = "https://search.yahoo.com/search?p=" + input;
+
+            HttpClient httpClient = new HttpClient();
+            string htmlYahoo = await httpClient.GetStringAsync(urlYahoo);
+
+            string toFindYahoo = "Next</a><span>";
+            int startYahoo = htmlYahoo.IndexOf(toFindYahoo) + toFindYahoo.Length;
+            int endYahoo = startYahoo + 40;
+            string resultYahoo = htmlYahoo.Substring(startYahoo, endYahoo - startYahoo);
+
+            string onlyNumberYahoo = Regex.Replace(resultYahoo, "[a-zA-Z</>,\n. ]", "");
+            inputYahoo = Convert.ToInt32(onlyNumberYahoo);
+
+            table.Rows[0][0] = inputYahoo;
+            
+            //----Search on Bind
+            string urlBind = "https://www.bing.com/search?q=" + input;
+            httpClient = new HttpClient();
+            string htmlBind = await httpClient.GetStringAsync(urlBind);
+
+            string toFindBind = "class=\"sb_count\"";
+            int startBind = htmlBind.IndexOf(toFindBind) + toFindBind.Length;
+            int endBind = startBind + 40;
+            string resultBind = htmlBind.Substring(startBind, endBind - startBind);
+
+            string onlyNumberBind = Regex.Replace(resultBind, "[a-zA-Z</>,\n. ]", "");
+            inputBind = Convert.ToInt32(onlyNumberBind);
+
+            table.Rows[0][1] = inputBind;
+            //---Compare the result of the two search engines
+            if (inputYahoo> inputBind)
+            {
+                table.Rows[0][2] = "Yahoo";
+            }
+            else
+            {
+                table.Rows[0][2] = "Bind";
+            }
+
+            
+        }
+    }
+}
